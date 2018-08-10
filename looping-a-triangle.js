69151db5e4d5fa46b0787e05ed868d074982b686
Valia Dempsey <valentinaml@gmail.com>
2018-08-09 23:20:18 -0700

    Add solution for FizzBuzz exercise

--- looping-a-triangle.js
+++ looping-a-triangle.js
@@ -1,4 +1,4 @@
-//solution A
+//Triangle solution A
 
 var x = "";
 for (i = 0; i < 7; i++) {
@@ -7,8 +7,25 @@ for (i = 0; i < 7; i++) {
 };
 
 
-//solution B (book)
+//Triangle solution B (book)
 
 for (var line = "#"; line.length < 8; line += "#") {
   console.log(line);
 };
+
+//FizzBuzz solution
+
+for (var number=1; number <= 100; number++) {
+  if (number % 15 === 0) {
+  	console.log("FizzBuzz");
+  } else if (number % 5 === 0) {
+  	console.log("Buzz");
+  } else if (number % 3 === 0) {
+  	console.log("Fizz");
+  } else {
+    console.log(number);
+  }  
+};
+
+
+
