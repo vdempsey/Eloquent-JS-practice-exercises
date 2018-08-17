814db2e3015035db136bfc3cdcfa84dbaca27b6d
Valia Dempsey <valentinaml@gmail.com>
2018-08-17 11:32:33 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Eloquent JavaScript Exercises
+_JavaScript refresh_
+
+
+### Exercises:
+##### CH.2 PROGRAM STRUCTURE
+1. Looping a Triangle
+2. FizzBuzz
+3. Chess Board
+
+##### CH.3 FUNCTIONS
+1. Minimum
+2. Recursion
+3. Bean Counting
--- js-exercises.js
+++ js-exercises.js
@@ -122,10 +122,8 @@ function countChar (string, char) {
 }
 
 function countBs (string) {
-  	return countChar(string, "B");
+  return countChar(string, "B");
 }
 
 console.log(countBs("BBC"));
-// → 2
 console.log(countChar("kakkerlak", "k"));
-// → 4
